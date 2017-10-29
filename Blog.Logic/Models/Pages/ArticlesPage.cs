df9c9f3d19f05424884f37ff126aef54cd652206
ines <ines.bielecka@gmail.com>
2017-10-30 00:50:44 +0100

    create new pages and make link between them

---
+++ Blog.Logic/Models/Pages/ArticlesPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Our.Umbraco.Ditto;
+
+namespace Blog.Logic.Models.Pages
+{
+    public class ArticlesPage
+    {
+        public string ArticleInfo { get; set; }
+        public string ArticleContent { get; set; }
+    }
+}
